f0e403755791abdda4fca26db095fc03ff8f87a7
Tatsuo Fukushima <tatsuo.fukushima@fxat.co.jp>
2014-01-14 15:14:04 +0900

    Refactoring
    add parameter

--- src/GitGit.java
+++ src/GitGit.java
@@ -5,7 +5,7 @@ public class GitGit {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Helper.sayHelloTwice();
+		Helper.sayHello(2);
 	}
 
 }
--- src/Helper.java
+++ src/Helper.java
@@ -1,8 +1,8 @@
 
 public final class Helper {
-	public static void sayHelloTwice(){
-		System.out.println("Hello GitGit!");
-		System.out.println("Hello GitGit!");
-		
+	public static void sayHello(int count){
+		while(0 <= count--){
+			System.out.println("Hello GitGit!");
+		}
 	}
 }
